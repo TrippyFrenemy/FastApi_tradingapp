bac026686e6d9eba5d3f47229f963e05caf4b7ac
Аврущенко Дмитро Мансорович <trippyfren@knu.ua>
2023-07-31 18:35:18 +0300

    [upd] Trying to understand mapping and builtin api docs

---
+++ README.md
@@ -0,0 +1,8 @@
+# App using FastApi Framework
+This project was created so that I could learn a new framework in it
+
+
+## To start project
+```bash
+ uvicorn main:app --reload
+```
--- main.py
+++ main.py
@@ -5,6 +5,13 @@ app = FastAPI(
 )
 
 
-@app.get("/")
-def hello():
-    return "Hello world"
+fake_db = [
+    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 2, "role": "investor", "name": "John"},
+    {"id": 3, "role": "trader", "name": "Alice"},
+]
+
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return [user for user in fake_db if user.get("id") == user_id]
