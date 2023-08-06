2cb1241acb4100ee75620f6c21410bb380a19003
Аврущенко Дмитро Мансорович <trippyfren@knu.ua>
2023-08-06 12:19:31 +0300

    [upd] Trying orm queries and statements using sqlalchemy(for orm) and asyncpg(as engine for connection)

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from .auth.base_config import auth_backend, fastapi_users
 from .auth.schemas import UserRead, UserCreate
 
+from .operations.router import router as router_operation
 
 app = FastAPI(
     title="Trading App"
@@ -20,3 +21,8 @@ app.include_router(
     tags=["Auth"],
 )
 
+app.include_router(
+    router_operation,
+    prefix="/operations",
+    tags=["Operation"]
+)
